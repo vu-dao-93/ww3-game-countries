e057b9c678ba809a1417dc9a9cdb00a0e9b447dc
Vu Dao <anhvudao17@gmail.com>
2018-03-21 15:57:56 +0200

    Revert "Revert "New design for API, get random Allies""

--- app/countries/router.js
+++ app/countries/router.js
@@ -3,31 +3,55 @@ import countries from './model';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const query = req.query;
+  const params = req.query;
+  console.log(params);
   try {
-    if (query.q) {
-      res.send(await countries.search(query.q));
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(error.status).send(error.message);
-  }
-});
-
-router.get('/hqs', async (req, res) => {
-  try {
-    const hqs = await countries.getHqs();
-    res.json(hqs);
-  } catch (error) {
-    console.log(error);
-    res.status(error.status).send(error.message);
-  }
-});
-
-router.get('/hqs/r', async (req, res) => {
-  try {
-    const results = await countries.randomHq();
+    let { specials, ipp, pg, sort } = params;
+    ipp = +ipp;
+    pg = +pg;
+    let query = {};
+    Object.keys(params).forEach(key => {
+      if (['ipp', 'pg', 'sort'].indexOf(key) > -1) {
+        return;
+      }
+      const tableKey = key.charAt(0).toUpperCase() + key.substr(1);
+      query[tableKey] = params[key];
+    })
+    const results = await countries.get({
+      query,
+      sort,
+      ipp,
+      pg
+    });
+    console.log(results);
     res.json(results);
+  } catch (error) {
+    console.log(error);
+    res.status(error.status).send(error.message);
+  }
+});
+
+router.get('/search', async (req, res) => {
+  const params = req.query;
+  let { keyword, field, sort, ipp, pg } = params;
+  keyword = keyword.split(',');
+  field = field.split(',');
+  ipp = +ipp;
+  pg = +pg;
+  let query = {};
+  field.forEach((element, index) => {
+    query[element] = {
+      $regex: keyword[index],
+      $options: 'i'
+    }
+  });
+  try {
+    res.json(await countries.search({
+      query,
+      sort,
+      ipp,
+      pg
+    }));
   } catch (error) {
     console.log(error);
     res.status(error.status).send(error.message);
